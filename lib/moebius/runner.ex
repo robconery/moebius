53ba9322e0cc3eae5d70450d40455e4a89720d64
Johnny Winn <j.winn.v@gmail.com>
2015-11-04 00:37:03 -0500

    Convert Runner to GenServer to broker connections

--- lib/moebius/runner.ex
+++ lib/moebius/runner.ex
@@ -1,4 +1,14 @@
+defmodule Connection do
+  defstruct pid: nil, locked: false
+end
+
 defmodule Moebius.Runner do
+  use GenServer
+
+  def start_link do
+    GenServer.start_link(__MODULE__, fill_pool, name: Moby)
+  end
+
   @moduledoc """
   The main execution bits are in here.
   """
@@ -6,26 +16,80 @@ defmodule Moebius.Runner do
   @doc """
   Spawn a Postgrex worker to run our query using the config specified in /config
   """
-  def connect do
+  defp fill_pool do
     extensions = [{Postgrex.Extensions.JSON, library: Poison}]
-    
-    Application.get_env(:moebius, :connection)
-    |> Keyword.update(:extensions, extensions, &(&1 ++ extensions))
-    |> Postgrex.Connection.start_link
+
+    Enum.map(1..5, fn(_n) ->
+      {:ok, pid} = Application.get_env(:moebius, :connection)
+                |> Keyword.update(:extensions, extensions, &(&1 ++ extensions))
+                |> Postgrex.Connection.start_link
+
+      %Connection{pid: pid}
+    end)
   end
 
   @doc """
   If there isn't a connection process started then one is added to the command
   """
   def execute(cmd) do
-    {:ok, pid} = connect()
-    try do
-      case Postgrex.Connection.query(pid, cmd.sql, cmd.params) do
-        {:ok, result} -> {:ok, result}
-        {:error, err} -> {:error, err.postgres.message}
-      end
+    GenServer.call(Moby, {:execute, cmd})
+  end
+
+  def connect do
+    GenServer.call(Moby, {:connect})
+  end
+
+  def handle_call({:execute, cmd}, _from, state) do
+    {pid, results} = query(cmd, state)
+    unlock(pid, state)
+    send(self, {:unlocked, pid})
+    {:reply, {:ok, results}, state}
+  end
+
+  def handle_call({:connect}, _from, state) do
+    {:reply, {:ok, available_connection(state)}, state}
+  end
+
+  defp unlock(_open_id, []), do: []
+
+  defp unlock(open_pid, [%{pid: pid, locked: true}|pool]) when open_pid == pid do
+    [%{pid: open_pid, locked: false}|pool]
+  end
+
+  defp unlock(open_pid, [_pid|pool]),
+    do: unlock(open_pid, pool)
+
+  defp available_connection([]) do
+    receive do
+      {:unlocked, pid} ->
+        pid
     after
-      Postgrex.Connection.stop(pid)
+      100 -> raise "No available connections"
+    end
+  end
+
+  defp available_connection([%{pid: _pid, locked: true}|pool]),
+    do: available_connection(pool)
+
+  defp available_connection([%{pid: pid, locked: false}|_pool]),
+    do: pid
+
+  defp query(cmd, []) do
+    receive do
+      {:unlocked, pid} ->
+        query(cmd, [%{pid: pid, locked: false}])
+    after
+      100 -> raise "Error querying database"
+    end
+  end
+
+  defp query(cmd, [%{pid: _pid, locked: true}|pool]),
+    do: query(cmd, pool)
+
+  defp query(cmd, [%{pid: pid, locked: false}|_pool]) do
+    case Postgrex.Connection.query(pid, cmd.sql, cmd.params) do
+      {:ok, result} -> {pid, result}
+      {:error, err} -> {:error, err.postgres.message}
     end
   end
 
@@ -44,16 +108,15 @@ defmodule Moebius.Runner do
     end
   end
 
-
   def open_transaction() do
-    {:ok, pid} = Moebius.Runner.connect()
+    {:ok, pid} = Moebius.Runner.connect
+
     Postgrex.Connection.query(pid, "BEGIN;",[])
     pid
   end
 
   def commit_and_close_transaction(pid) do
     Postgrex.Connection.query(pid, "COMMIT;",[])
-    Postgrex.Connection.stop(pid)
   end
 
   @doc """
--- test/moebius/connection_release_test.exs
+++ test/moebius/connection_release_test.exs
@@ -1,99 +1,45 @@
 defmodule Moebius.ConnectionReleaseTest do
   use ExUnit.Case
-  #import Moebius.Query
 
-  # setup do
-  #   "delete from users cascade;" |> Moebius.Query.run
-  #   "delete from people"
-  #     |> Moebius.Query.run
-  #   {:ok, res: true}
-  # end
+  import Moebius.Query
 
-  # test "loading 100 at once" do
-  #   res = write_benchmark(100)
-  #   IO.puts "Finished"
-  # end
-  #
-  # test "loading 1000 at once" do
-  #   res = write_benchmark(1000)
-  #   IO.puts "Finished"
-  # end
+   setup do
+     "delete from people;" |> Moebius.Query.run
+     {:ok, res: true}
+   end
 
-  # test "50K inserts with psql" do
-  #   cmds = user_pulls(10_000_000)
-  #   sql = Enum.join(cmds, ";")
-  #   res = Moebius.Runner.run_with_psql(sql, "meebuss")
-  #   IO.inspect res
+  test "loading 100 at once" do
+    assert 100 = write_benchmark(100) |> Enum.count
+  end
+
+  test "loading 1000 at once" do
+    assert 1000 = write_benchmark(1000) |> Enum.count
+  end
+
+  # This passes but does slow the test suite down
+  # test "loading 10000 at once" do
+  #   assert 10000 = write_benchmark(10000) |> Enum.count
   # end
 
-  # test "fifty thousand writes" do
-  #   "delete from users cascade;" |> Moebius.Query.run
-  #   cmds = user_writes(50000)
-  #   sql = Enum.join(cmds, ";\r\n")
-  #   file = write_batch_to_file(sql)
-  #   #res = System.cmd "psql", "-d", "meebuss", "-f", "db.sql"
-  #   #IO.inspect sql
-  #   res = Moebius.Runner.run_file_with_psql(file, "meebuss")
-  #   IO.inspect res
-  # end
+  defp people(qty) do
+    Enum.map(1..qty, &(
+      [
+        first_name: "FirstName #{&1}",
+        last_name: "LastName #{&1}",
+        address: "666 SW Pine St.",
+        city: "Portland",
+        state: "OR",
+        zip: "97209" ]))
+  end
 
-  # test "50K concurrent writes" do
-  #   Enum.map(1..200, fn(n) ->
-  #     sql="insert into users(first, last email) values($1, $2, $3);"
-  #     user = [email: "test#{n}@test.com", first: "First #{n}", last: "Last #{n}"]
-  #     cmd = %Moebius.QueryCommand{table_name: "users" }
-  #     res = spawn(Moebius.Query, :insert, [cmd, user])
-  #     end
-  #   )
-  #   IO.puts "Done"
-  # end
+  defp write_benchmark(qty) do
+    people(qty)
+    |> Enum.map(&save/1)
+  end
 
-  # def user_write_commands(qty) do
-  #   Enum.map(1..qty, fn(n) ->
-  #       db(:users)
-  #         |> insert(email: "test#{n}@test.com", first: "First #{n}", last: "Last #{n}")
-  #     end
-  #   )
-  # end
-  #
-  # def user_write_sql(qty) do
-  #   Enum.map(1..qty, fn(n) ->
-  #       "insert into users(first, last, email) values('first #{n}', 'last #{n}', 'test#{n}@test.com')"
-  #     end
-  #   )
-  # end
-  #
-  # def user_pulls(qty) do
-  #   Enum.map(1..qty, fn(n) ->
-  #       "select * from users where id= 1"
-  #     end
-  #   )
-  # end
-  #
-  # def write_batch_to_file(sql_blob) do
-  #   build_file = "db.sql"
-  #   {:ok, file} = File.open build_file, [:write]
-  #   IO.binwrite file, sql_blob
-  #   File.close file
-  #   build_file
-  # end
-
-  # def people(qty) do
-  #   Enum.map(1..qty, &(
-  #     [
-  #       first_name: "FirstName #{&1}",
-  #       last_name: "LastName #{&1}",
-  #       address: "666 SW Pine St.",
-  #       city: "Portland",
-  #       state: "OR",
-  #       zip: "97209" ]))
-  # end
-  #
-  # def write_benchmark(qty) do
-  #   people(qty)
-  #   |> Enum.map(fn(p) ->
-  #     db(:people)
-  #       |> save(p)
-  #   end)
-  # end
+  defp save(record) do
+    db(:people)
+    |> insert_command(record)
+    |> execute
+  end
 end
