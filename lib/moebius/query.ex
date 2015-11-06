819e19e99da416a6494d88354e5bb75d31af9699
xivSolutions <xivSolutions@gmail.com>
2015-11-05 21:19:06 -0700

    implement bulk insert in master w/o broker

--- lib/moebius/query.ex
+++ lib/moebius/query.ex
@@ -486,6 +486,53 @@ defmodule Moebius.Query do
   end
 
 
+def insert(cmd, [[h | t] | rest]) do
+    records = [[h | t] | rest]
+    [first | rest] = records
+    transaction fn(pid) ->
+      bulk_insert_batch(cmd, records, [])
+      |> Enum.map(fn(cmd) -> execute(cmd, pid) end)
+      |> List.flatten        
+    end
+  end
+
+   defp bulk_insert_batch(cmd, records, acc) do
+    [first | rest] = records
+
+    # 20,000 seems to be the optimal number here. Technically you can go up to 34,464, but I think Postgrex imposes a lower limit, as I
+    # hit a wall at 34,000, but succeeded at 30,000. Perf on 100k records is best at 20,000. 
+    max_params = 20000 
+    cmd = %{ cmd | columns: Keyword.keys(first)}
+    max_records_per_command = div(max_params, length(cmd.columns))
+    
+    { current, next_batch } = Enum.split(records, max_records_per_command)
+    this_cmd = bulk_insert_command(cmd, current)
+    case next_batch do
+      [] -> Enum.reverse([this_cmd | acc])
+      _ -> 
+        db(cmd.table_name) |> bulk_insert_batch(next_batch, [this_cmd | acc])
+    end
+  end
+
+
+  defp bulk_insert_command(cmd, [first | rest]) do
+    records = [first | rest]
+    cols = cmd.columns
+
+    vals = Enum.reduce(Enum.reverse(records), [], fn(listitem, acc) -> 
+    Enum.concat(Keyword.values(listitem), acc) end)
+
+    params_sql = elem(Enum.map_reduce(vals, 0, fn(v, acc) -> {"$#{acc + 1}", acc + 1} end),0)
+    |> Enum.chunk(length(cols))
+    |> Enum.map(fn(chunk) -> "(#{Enum.join(chunk, ", ")})" end)
+    |> Enum.join(", ")
+
+    sql_body = "insert into #{cmd.table_name} (" <> Enum.join(cols, ", ") <> ") " <>
+    "values #{ params_sql } returning *;"
+
+    %{cmd | columns: cols, sql: sql_body, params: vals, type: :insert}
+  end
+
   @doc """
   A simple insert that is part of a transaction that returns the inserted record. Create your list of data and send it on in.
 
