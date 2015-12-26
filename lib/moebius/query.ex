4b1cd3da87bc5fb37c690b13c8ff61d256abe88f
Paul Lamb <paul@oil-law.com>
2015-12-26 14:19:45 -0600

    reorder insert/2 and remove unused variables to remove compiler warnings

--- lib/moebius/query.ex
+++ lib/moebius/query.ex
@@ -418,38 +418,22 @@ defmodule Moebius.Query do
 
   end
 
-  defp bulk_insert_batch(%QueryCommand{} = cmd, records, acc, column_map) do
+  @doc """
+  A simple insert that that returns the inserted record. Create your list of data and send it on in.
 
-    # 20,000 seems to be the optimal number here. Technically you can go up to 34,464, but I think Postgrex imposes a lower limit, as I
-    # hit a wall at 34,000, but succeeded at 30,000. Perf on 100k records is best at 20,000.
-    max_params = 20000
-    cmd = %{ cmd | columns: column_map}
-    max_records_per_command = div(max_params, length(cmd.columns))
+  criteria:   -    A list or map of data to be saved
 
-    { current, next_batch } = Enum.split(records, max_records_per_command)
-    this_cmd = bulk_insert_command(cmd, current)
-    case next_batch do
-      [] -> Enum.reverse([this_cmd | acc])
-      _ ->
-        db(cmd.table_name) |> bulk_insert_batch(next_batch, [this_cmd | acc], column_map)
-    end
-  end
+  Example:
 
-  defp bulk_insert_command(%QueryCommand{} = cmd, [first | rest]) do
-    records = [first | rest]
-    cols = cmd.columns
-    vals = Enum.reduce(Enum.reverse(records), [], fn(listitem, acc) ->
-      Enum.concat(Keyword.values(listitem), acc) end)
-
-    params_sql = elem(Enum.map_reduce(vals, 0, fn(v, acc) -> {"$#{acc + 1}", acc + 1} end),0)
-    |> Enum.chunk(length(cols), length(cols), [])
-    |> Enum.map(fn(chunk) -> "(#{Enum.join(chunk, ", ")})" end)
-    |> Enum.join(", ")
-
-    sql_body = "insert into #{cmd.table_name} (" <> Enum.join(cols, ", ") <> ") " <>
-    "values #{ params_sql } returning *;"
-
-    %{cmd | columns: cols, sql: sql_body, params: vals, type: :insert}
+  ```
+  new_user = db(:users)
+      |> insert(email: "test@test.com", first: "Test", last: "User")
+  ```
+  """
+  def insert(%QueryCommand{} = cmd, criteria) do
+    cmd
+    |> insert_command(criteria)
+    |> execute(:single)
   end
 
   @doc """
@@ -474,24 +458,6 @@ defmodule Moebius.Query do
   end
 
   @doc """
-  A simple insert that that returns the inserted record. Create your list of data and send it on in.
-
-  criteria:   -    A list or map of data to be saved
-
-  Example:
-
-  ```
-  new_user = db(:users)
-      |> insert(email: "test@test.com", first: "Test", last: "User")
-  ```
-  """
-  def insert(%QueryCommand{} = cmd, criteria) do
-    cmd
-    |> insert_command(criteria)
-    |> execute(:single)
-  end
-
-  @doc """
   Creates an insert command based on the assembled pipeline
   """
   def insert_command(%QueryCommand{} = cmd, criteria) do
@@ -501,6 +467,40 @@ defmodule Moebius.Query do
     " values(" <> Enum.map_join(1..length(cols), ", ", &"$#{&1}") <> ") returning *;"
 
     %{cmd | sql: sql, params: vals, type: :insert}
+  end
+
+  defp bulk_insert_batch(%QueryCommand{} = cmd, records, acc, column_map) do
+
+    # 20,000 seems to be the optimal number here. Technically you can go up to 34,464, but I think Postgrex imposes a lower limit, as I
+    # hit a wall at 34,000, but succeeded at 30,000. Perf on 100k records is best at 20,000.
+    max_params = 20000
+    cmd = %{ cmd | columns: column_map}
+    max_records_per_command = div(max_params, length(cmd.columns))
+
+    { current, next_batch } = Enum.split(records, max_records_per_command)
+    this_cmd = bulk_insert_command(cmd, current)
+    case next_batch do
+      [] -> Enum.reverse([this_cmd | acc])
+      _ ->
+        db(cmd.table_name) |> bulk_insert_batch(next_batch, [this_cmd | acc], column_map)
+    end
+  end
+
+  defp bulk_insert_command(%QueryCommand{} = cmd, [first | rest]) do
+    records = [first | rest]
+    cols = cmd.columns
+    vals = Enum.reduce(Enum.reverse(records), [], fn(listitem, acc) ->
+      Enum.concat(Keyword.values(listitem), acc) end)
+
+    params_sql = elem(Enum.map_reduce(vals, 0, fn(_, acc) -> {"$#{acc + 1}", acc + 1} end),0)
+    |> Enum.chunk(length(cols), length(cols), [])
+    |> Enum.map(fn(chunk) -> "(#{Enum.join(chunk, ", ")})" end)
+    |> Enum.join(", ")
+
+    sql_body = "insert into #{cmd.table_name} (" <> Enum.join(cols, ", ") <> ") " <>
+    "values #{ params_sql } returning *;"
+
+    %{cmd | columns: cols, sql: sql_body, params: vals, type: :insert}
   end
 
   @doc """
