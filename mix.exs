dab8d27b5bc8aeea3f4b6050027f228740d0b43a
Paul Lamb <paul@oil-law.com>
2015-12-28 15:06:05 -0600

    update dependencies to current, except for postgrex

--- mix.exs
+++ mix.exs
@@ -30,12 +30,12 @@ defmodule Moebius.Mixfile do
 
   defp deps do
     [{:postgrex, "~> 0.9.1"},
-     {:timex, "~> 0.19.4"},
+     {:timex, "~> 1.0.0-rc4"},
      {:inflex, "~> 1.5.0"},
      {:poison, "~> 1.5"},
      {:json, "~> 0.3.0"},
-     {:ex_doc, "~> 0.10", only: [:dev, :docs]},
-     {:earmark, "~> 0.1.18", only: [:dev, :docs]},
+     {:ex_doc, "~> 0.11.2", only: [:dev, :docs]},
+     {:earmark, "~> 0.2.0", only: [:dev, :docs]},
      {:credo, "~> 0.2", only: [:dev, :test]}]
   end
 
