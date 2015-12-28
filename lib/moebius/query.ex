caf13336aa3b02d6b1e56d5db536d22b7a3a90ea
Paul Lamb <paul@oil-law.com>
2015-12-28 15:02:31 -0600

    redo the spaces so that tests pass

--- lib/moebius/query.ex
+++ lib/moebius/query.ex
@@ -524,7 +524,7 @@ defmodule Moebius.Query do
         {filters, _count} = Enum.map_reduce cmd.where_columns, col_count, fn col, acc ->
           {"#{col} = $#{acc}", acc + 1}
         end
-        "where " <> Enum.join(filters, " and ")
+        " where " <> Enum.join(filters, " and ")
 
       cmd.where -> cmd.where
     end
@@ -537,7 +537,7 @@ defmodule Moebius.Query do
 
     columns = Enum.join(cols, ", ")
 
-    sql = "update #{cmd.table_name} set #{columns} #{where} returning *;"
+    sql = "update #{cmd.table_name} set #{columns}#{where} returning *;"
     %{cmd | sql: sql, type: :update, params: params}
   end
 
