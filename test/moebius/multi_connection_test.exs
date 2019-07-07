0cb4419804180767ccac1ac51d6ad1d27c3dc163
Krithik Rao <krdevmail@gmail.com>
2019-07-07 15:47:34 -0400

    Updated Corresponding Tests
    Updated the tests to not use Supervisor.Spec.worker/3, and instead
    relies on using the defined child_spec/1 in each Moebius.Database.

--- test/moebius/multi_connection_test.exs
+++ test/moebius/multi_connection_test.exs
@@ -8,9 +8,11 @@
 #   import Moebius.Query
 #
 #   setup_all do
-#     w1 = Supervisor.Spec.worker(DB1, [Moebius.get_connection(:test_db)])
-#     w2 = Supervisor.Spec.worker(DB2, [Moebius.get_connection(:chinook)])
-#     Supervisor.start_link [w1, w2], strategy: :one_for_one
+#     children = [
+#       {DB1, [Moebius.get_connection(:test_db)]}
+#       {DB2, [Moebius.get_connection(:chinook)]}
+#     ]
+#     Supervisor.start_link children, strategy: :one_for_one
 #
 #     {:ok, [thing: true]}
 #   end
